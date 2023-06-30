f285906d4bd99e59f64dc0f8b6836748984fadd7
Y Huang <yulih@andrew.cmu.edu>
2023-06-30 13:38:35 -0400

    Week-1-end

---
+++ sql/Week1_raw_data_schemas.sql
@@ -0,0 +1,16 @@
+--By Yuli Huang, Start 06/29/2023
+.echo ON
+--output results to text file in repo
+.output C:\CMU\2023_S2\95797_DataWarehousing\Class_Project\CMU-95797-Project\answers\Week1_raw_schemas.txt
+
+--Check the table lists
+SHOW TABLES;
+
+--Check each of the TABLES
+DESCRIBE bike_data;
+DESCRIBE central_park_weather;
+DESCRIBE daily_citi_bike_trip_counts_and_weather;
+DESCRIBE fhv_bases;
+DESCRIBE fhvhv_tripdata;
+DESCRIBE green_tripdata;
+DESCRIBE yellow_tripdata;
