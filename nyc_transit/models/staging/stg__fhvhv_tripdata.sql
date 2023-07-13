aebcb8e42553a011e9fcbd6d76a335fcdb40f8d3
Y Huang <yulih@andrew.cmu.edu>
2023-07-12 21:20:40 -0400

    More_flag_to_bool_change

--- nyc_transit/models/staging/stg__fhvhv_tripdata.sql
+++ nyc_transit/models/staging/stg__fhvhv_tripdata.sql
@@ -31,11 +31,16 @@ cleaned as (
         airport_fee,
         tips,
         driver_pay,
-        CASE WHEN shared_request_flag = 'Y' THEN TRUE ELSE FALSE END AS shared_request_flag,
-        CASE WHEN shared_match_flag = 'Y' THEN TRUE ELSE FALSE END AS shared_match_flag,
-        CASE WHEN access_a_ride_flag = ' ' THEN TRUE ELSE FALSE END AS access_a_ride_flag,
-        CASE WHEN wav_request_flag = 'Y' THEN TRUE ELSE FALSE END AS wav_request_flag,
-        CASE WHEN wav_match_flag = 'Y' THEN TRUE ELSE FALSE END AS wav_match_flag,
+        --CASE WHEN shared_request_flag = 'Y' THEN TRUE ELSE FALSE END AS shared_request_flag,
+        --CASE WHEN shared_match_flag = 'Y' THEN TRUE ELSE FALSE END AS shared_match_flag,
+        --CASE WHEN access_a_ride_flag = ' ' THEN TRUE ELSE FALSE END AS access_a_ride_flag,
+        --CASE WHEN wav_request_flag = 'Y' THEN TRUE ELSE FALSE END AS wav_request_flag,
+        --CASE WHEN wav_match_flag = 'Y' THEN TRUE ELSE FALSE END AS wav_match_flag,
+        {{flag_to_bool("shared_request_flag")}} as shared_request_flag,
+        {{flag_to_bool("shared_match_flag")}} as shared_match_flag,
+        {{flag_to_bool("access_a_ride_flag")}} as access_a_ride_flag,
+        {{flag_to_bool("wav_request_flag")}} as wav_request_flag,
+        {{flag_to_bool("wav_match_flag",)}} as wav_match_flag,
         filename
 	FROM source
 	
