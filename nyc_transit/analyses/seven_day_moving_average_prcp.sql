5ad5fc928b365ac00b176b5f5f4baac8043b72a4
Y Huang <yulih@andrew.cmu.edu>
2023-07-28 00:17:05 -0400

    Lead and Lag - Simple

---
+++ nyc_transit/analyses/seven_day_moving_average_prcp.sql
@@ -0,0 +1,12 @@
+SELECT 
+    DATE,
+    (LAG(PRCP, 3) OVER (ORDER BY DATE) 
+     + LAG(PRCP, 2) OVER (ORDER BY DATE) 
+     + LAG(PRCP, 1) OVER (ORDER BY DATE) 
+     + PRCP
+     + LEAD(PRCP, 1) OVER (ORDER BY DATE) 
+     + LEAD(PRCP, 2) OVER (ORDER BY DATE) 
+     + LEAD(PRCP, 3) OVER (ORDER BY DATE)) / 7.0 AS moving_avg_prcp
+FROM 
+    {{ ref('stg__central_park_weather')}}
+ORDER BY DATE
