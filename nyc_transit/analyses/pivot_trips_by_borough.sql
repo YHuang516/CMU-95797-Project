58c365570bdff1380dd233f040830b24ff9afb07
Y Huang <yulih@andrew.cmu.edu>
2023-07-27 23:29:06 -0400

    Pivot

---
+++ nyc_transit/analyses/pivot_trips_by_borough.sql
@@ -0,0 +1,6 @@
+SELECT
+    {{ dbt_utils.pivot('borough', 
+                        dbt_utils.get_column_values( ref('mart__fact_trips_by_borough'), 'borough'),
+                        then_value='trip_count'
+    )}}
+FROM {{ ref('mart__fact_trips_by_borough')}}
